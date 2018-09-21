187102588d650f4d06677b30f50fa312ba2151cc
w1zerd <43358171+w1zerd@users.noreply.github.com>
2018-09-20 21:19:57 -0700

    Update AnimatorControl.cs

--- FrostyLake/Assets/AnimatorControl.cs
+++ FrostyLake/Assets/AnimatorControl.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*Matthew Blaire September 2018*/
 public class AnimatorControl : MonoBehaviour {
 
     public Animator animator;
